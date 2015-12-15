46f3712ab19399b065073d2557b4aaf28238b17a
eliaskirsch <eliaskirsch@gmail.com>
2015-12-15 05:52:57 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(personname)
+    @name = personname
+  end
+  def name
+    @name
+  end
+  def job=(personjob)
+    @job = personjob
+  end
+  def job
+    @job
+  end
+end
